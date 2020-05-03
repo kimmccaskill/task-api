34c3db4ac48fa8dea5e1b4d61483e48a21500a3d
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-03 17:02:14 -0600

    Add endpoint to GET all users

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('/users', (req, res) => {
+  User.find({})
+  .then(users => res.send(users))
+  .catch(err => res.status(500).send())
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
 
