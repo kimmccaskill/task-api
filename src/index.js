855b8b06cf77c3cf12722b9d20f4551c5dcc6dfd
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-03 20:41:46 -0600

    Add async await endpoints for get and post

--- src/index.js
+++ src/index.js
@@ -8,28 +8,74 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body)
 
-  user.save()
-  .then(user => res.status(201).send(user))
-  .catch(err => {
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (err) {
     res.status(400).send(err)
-  })
+  }
 })
 
-app.get('/users', (req, res) => {
-  User.find({})
-  .then(users => res.send(users))
-  .catch(err => res.status(500).send())
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  } catch (err) {
+    res.status(500).send()
+  }
 })
 
-app.post('/tasks', (req, res) => {
+app.get('/users/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findById(_id)
+    if (!user) {
+      return res.status(404).send()
+    }
+    res.send(user)
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+// Tasks
+
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
-  task.save()
-  .then(task => res.status(201).send(task))
-  .catch(err => res.status(400).send(err))
+  try {
+    await task.save()
+    res.status(201).send(task)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.send(tasks)
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+  const _id = req.params.id
+
+  try {
+    const task = await Task.findById(_id)
+    if (!task) {
+      res.status(404).send()
+    }
+    res.send(task)
+  } catch (err) {
+    res.status(500).send()
+  }
 })
 
 app.listen(port, () => {
