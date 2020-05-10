0aec1ac921b0dfb2e6a97d45ce6b6290b9c4ed24
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-09 20:50:14 -0600

    Add logout functionality

--- src/routers/user.js
+++ src/routers/user.js
@@ -25,9 +25,20 @@ router.post('/users/login', async (req, res) => {
   }
 })
 
+
 router.post('/users/logout', auth, async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+    await req.user.save()
+    res.send()
+  } catch (err) {
+    res.status(500).send()
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
     await req.user.save()
 
     res.send()
