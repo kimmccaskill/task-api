6b6be9cae893729d6baea442123e3e8379a77350
Kim McCaskill <kimmccaskill92@gmail.com>
2020-05-14 14:26:02 -0600

    Import multer and add ability to upload files

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 const router  = new express.Router()
+
+
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
@@ -75,6 +78,13 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (err) {
     res.status(500).send()
   }
+})
+
+const upload = multer({
+  dest: 'avatars'
+})
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send()
 })
 
 module.exports = router
